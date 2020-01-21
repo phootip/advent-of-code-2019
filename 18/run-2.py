001471125ef1d5731120804d9a94b27d3f899ff6
phootip <phootip.t@gmail.com>
2020-01-21 21:43:17 +0700

    day 18 solved

--- 18/run-2.py
+++ 18/run-2.py
@@ -8,7 +8,7 @@ mem = {}
 
 def save_graph(data):
     x = y = 0
-    pos = ()
+    pos = []
     graph = {}
     for y in range(len(data)):
         for x in range(len(data[0])):
@@ -17,7 +17,7 @@ def save_graph(data):
             graph[(x, y)] = {'value': data[y][x], 'adj': []}
             if data[y][x] == '@':
                 graph[(x, y)]['value'] = '.'
-                pos = (x, y)
+                pos.append((x, y))
     return graph, pos
 
 def connect(graph):
@@ -53,7 +53,8 @@ def draw2(graph, pos):
             im.putpixel((x, y), ImageColor.getrgb('blue'))
         elif v['value'].islower():  # key
             im.putpixel((x, y), ImageColor.getrgb('red'))
-    im.putpixel(pos, ImageColor.getrgb('yellow'))
+    for p in pos:
+        im.putpixel(p, ImageColor.getrgb('yellow'))
     im.save('map.png')
 
 
@@ -107,34 +108,36 @@ def count_keys(graph):
             c += 1
     return c
 
-def part1(graph,pos,opened_doors):
-    if (pos,tuple(opened_doors)) in mem:
-        # print('big help')
-        return mem[(pos,tuple(opened_doors))]
+def len_sublist(l):
+    result = 0
+    for i in l:
+        result += len(i)
+    return result
+
+def part2(graph,pos_s,opened_doors):
+    # print(pos_s)
+    if (tuple(pos_s),tuple(opened_doors)) in mem:
+        return mem[(tuple(pos_s),tuple(opened_doors))]
     ans = float('inf')
-    keys = get_keys_cost(graph, pos)
-    # print(keys)
-    # print(f'len(keys): {len(keys)}')
-    if len(keys) > len(opened_doors):
-        for key in keys:
-            if set(key['doors']) <= opened_doors and graph[key['pos']]['value'].upper() not in opened_doors:
-                # print(graph[key['pos']]['value'])
-                new_pos = key['pos']
-                new_opened = opened_doors.copy()
-                new_opened.add(graph[key['pos']]['value'].upper())
-                # print(new_opened)
-                # print(key['cost'])
-                ans = min(ans,part1(graph,new_pos,new_opened) + key['cost'])
-            # if not d:
-            #     print(ans)
-        mem[(pos,tuple(opened_doors))] = ans
+    keys_4 = []
+    for pos in pos_s:
+        keys_4.append(get_keys_cost(graph, pos))
+    # print(keys_4)
+    if len_sublist(keys_4) > len(opened_doors):
+        for i in range(4):
+            keys = keys_4[i]
+            for key in keys:
+                if set(key['doors']) <= opened_doors and graph[key['pos']]['value'].upper() not in opened_doors:
+                    # print(graph[key['pos']]['value'])
+                    new_pos_s = pos_s[:]
+                    new_pos_s[i] = key['pos']
+                    new_opened = opened_doors.copy()
+                    new_opened.add(graph[key['pos']]['value'].upper())
+                    ans = min(ans,part2(graph,new_pos_s,new_opened) + key['cost'])
+        mem[(tuple(pos_s),tuple(opened_doors))] = ans
         return ans
-        # ending
-    # draw(graph, pos)
-    # draw2(graph, pos)
-    # print(ans)
     else:
-        mem[(pos,tuple(opened_doors))] = 0
+        mem[(tuple(pos_s),tuple(opened_doors))] = 0
         return 0
 
 
@@ -142,20 +145,20 @@ def run(data):
     height = len(data)
     width = len(data[0])
     # print(height,width)
-    graph, pos = save_graph(data)
+    graph, pos_s = save_graph(data)
     connect(graph)
-    draw2(graph, pos)
+    draw2(graph, pos_s)
     opened_doors = set()
     print(f'all_keys: {count_keys(graph)}')
-    ans = part1(graph,pos,opened_doors)
+    ans = part2(graph,pos_s,opened_doors)
     print(ans)
 
 
-for i in range(1, 6):
-    # if i == 4:
-    #     continue
-    # f = open(f'example{i}.txt')
-    f = open(f'input.txt')
+for i in range(1, 5):
+    ans = [6,24,32,72]
+    print(ans)
+    # f = open(f'example2-{i}.txt')
+    f = open(f'input2.txt')
     start = time()
     mem = {}
     data = list(map(lambda x: x.strip(), f.readlines()))
